10afaa7c44ded8ebc155677faf9a5f8a12276fe3
nainghazali-Kshahrewar-lab <nainghazali26@gmail.com>
2026-07-21 11:01:12 +0800

    Update Main.java

--- week_8-9/Main.java
+++ week_8-9/Main.java
@@ -1,23 +1,55 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        
+
+        Scanner input = new Scanner(System.in);
         ArrayList<String> tasks = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("===== ADD TASKS =====");
+
         for (int i = 1; i <= 3; i++) {
+
             System.out.print("Enter Task " + i + ": ");
-            String task = scanner.nextLine();
+            String task = input.nextLine();
+
             tasks.add(task);
         }
 
-        
-        System.out.println("\n===== TASK LIST =====");
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println((i + 1) + ". " + tasks.get(i));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("task.txt"))) {
+
+            for (String task : tasks) {
+                writer.write(task);
+                writer.newLine();
+            }
+
+            System.out.println("\nTasks saved successfully.\n");
+
+        } catch (IOException e) {
+
+            System.out.println(e.getMessage());
+
+        }
+
+        System.out.println("===== TASKS LOADED FROM FILE =====");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("task.txt"))) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                System.out.println(line);
+
+            }
+
+        } catch (IOException e) {
+
+            System.out.println(e.getMessage());
+
         }
 
         scanner.close();
